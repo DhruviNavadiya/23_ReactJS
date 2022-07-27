04f101a808d78b7bc1866caf20abd026c8bd5ff6
Dhruvi <navadiyadhruvi14@gmail.com>
2022-07-27 09:25:27 +0530

    dd

---
+++ JavaScript/script.js
@@ -0,0 +1,21 @@
+
+
+
+
+for(let counter = 1; counter <= 10; counter++){
+    document.write("TechGun");
+}
+
+//while(counter <= 10);
+
+// let counter = 1;
+// let sum =0;
+
+//  while(counter <=1000){
+//     if(counter % 2 == 0){
+//         sum = sum + counter;
+//     }
+//     counter ++;
+//  }
+
+//  document.write(sum);
